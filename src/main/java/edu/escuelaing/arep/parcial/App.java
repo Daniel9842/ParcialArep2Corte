5cb9527ef098232a505f5f1449849022059dc1c6
Daniel Ducuara <daniel.ducuara@mail.escuelaing.edu.co>
2021-10-13 13:57:54 -0500

    change pom and code

--- src/main/java/edu/escuelaing/arep/parcial/App.java
+++ src/main/java/edu/escuelaing/arep/parcial/App.java
@@ -1,13 +1,19 @@
 package edu.escuelaing.arep.parcial;
 
-/**
- * Hello world!
- *
- */
+import static spark.Spark.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        port(getPort());
+
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
     }
 }
